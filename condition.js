0c796f83f21472e305d2b3a508e346e9994262f2
tamimhasan7 <tamimhasan7038@gmail.com>
2025-06-23 00:58:22 +0600

    class-19 assignment

---
+++ condition.js
@@ -0,0 +1,47 @@
+// there are two types of conditions in JavaScript like if and else
+//example 1
+// const age = 18;
+
+// if (age < 18 ) {
+//     console.log("you are not eligible to vote");
+// } else {
+//     console.log("you are eligible to vote")
+// }
+
+
+
+//example 2
+// const age = 16;
+
+// if (age < 18 ) {
+//     console.log("you are not eligible to vote");
+// } else {
+//     console.log("you are eligible to vote")
+// }
+
+
+//example 3
+// const marks = -4;
+// if (marks >= 80) {
+//     console.log("You got A+ grade");
+// } else if (marks >= 70) {
+//     console.log("You got A grade")
+// } else if (marks >= 60) {
+//     console.log("You got B grade")
+// } else if (marks >= 50) {
+//     console.log("You got C grade")
+// } else if (marks >= 40) {
+//     console.log("You got D grade")
+// } else if (marks >= 33) {
+//     console.log("You got E grade")
+// } else {
+//     console.log("You failed")
+// }
+
+
+//example 4
+const marks = 98;
+if (marks >= 80) {
+    console.log("You got A+ grade")
+} 
+
