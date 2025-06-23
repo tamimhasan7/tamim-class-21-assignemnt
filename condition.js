46d34750739af31b1f86876e919039c2a1f41336
tamimhasan7 <tamimhasan7038@gmail.com>
2025-06-23 21:59:30 +0600

    Add files via upload

--- condition.js
+++ condition.js
@@ -40,8 +40,76 @@
 
 
 //example 4
-const marks = 98;
-if (marks >= 80) {
-    console.log("You got A+ grade")
-} 
+// const marks = 110;
+// if (marks <= 100 && marks >= 80) {
+//     console.log("You got A+ grade");
+// } else if (marks <= 79 && marks >= 70) {
+//     console.log("You got A grade");
+// } else if (marks <= 69 && marks >= 60) {
+//     console.log("You got B grade");
+// } else if (marks <= 59 && marks >= 50) {
+//     console.log("You got C grade")
+// } else if (marks <= 49 && marks >= 40) {
+//     console.log("You got D grade")
+// } else if (marks <= 39 && marks >= 33) {
+//     console.log("You got E grade")
+// } else {
+//     console.log("You failed")
+// }
 
+
+//example 5
+// const run = 334;
+// if (run <= 0) {
+//     console.log("You got a duck");
+// } else if (run <= 19 && run >= 0) {
+//     console.log("You are a tale-ender batsman");
+// } else if (run <= 39 && run >= 20) {
+//     console.log("You are mediocre batsman");
+// } else if (run <= 59 && run >= 40) {
+//     console.log("You are a good batsman");
+// } else if (run <= 99 && run >= 60) {
+//     console.log("You are one of the best batsmen");
+// } else {
+//     console.log("You are a legendary batsman or you don't play cricket at all")
+// }
+
+
+//examle 6
+// const age = 80;
+// if (age <= 1 && age >= 0) {
+//     console.log("It is called an infant");
+// } else if (age <= 4 && age >= 6) {
+//     console.log("He/She is called a toddler");
+// } else if (age <= 9 && age >= 7) {
+//     console.log("He/She is preschooler");
+// } else if (age <= 14 && age>=12) {
+//     console.log("He/She is a primary schooler");
+// } else if (age <= 25 && age >= 18) {
+//     console.log("A human is called an adolescent or a teenager");
+// } else if (age <= 35 && age >= 30) {
+//     console.log("A human is an adult regrardless of his/her gender");
+// } else if (age <= 55 && age >= 40) {
+//     console.log("A human is in middle adulthood");
+// } else {
+//     console.log("A human is called an old person")
+// }
+
+
+//example 7
+// const price = 100000;
+// if (price < 100) {
+//     console.log("It is called cheap");
+// } else if (price > 100 && price < 500) {
+//     console.log("It is an affordable product")
+// } else if (price > 500 && price < 1000) {
+//     console.log("It is product for middle class"); 
+// } else if (price > 1000 && price < 2000) {
+//     console.log("It is product for upper-middle class");
+// } else {
+//     console.log("It is a product for upper class")
+// }
+
+
+//example 8
+// const 
