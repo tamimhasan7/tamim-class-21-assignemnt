eae585f89c93840bfa377a97a54acda27b940b6b
tamimhasan7 <tamimhasan7038@gmail.com>
2025-07-08 12:50:12 +0600

    Add files via upload

---
+++ for.js
@@ -0,0 +1,60 @@
+// console.log("Serial No.", 1);
+// console.log("Serial No.", 2);
+// console.log("Serial No.", 3);
+// console.log("Serial No.", 4);
+// console.log("Serial No.", 5);
+// console.log("Serial No.", 6);
+// console.log("Serial No.", 7);
+// console.log("Serial No.", 8);
+// console.log("Serial No.", 9);
+// console.log("Serial No.", 10);
+
+// Example 1
+// for (let count = 1; count <= 1000; count++) {
+//   console.log("Your serial number is:", count);
+// }
+
+// Example 2
+// for (let count = 1; count <= 100; count++) {
+//   if (count % 2 == 0) {
+//     console.log("This is an even number: ", count);
+//   }
+// }
+
+// console.log("=============================");
+
+//Example 3
+// for (let count = 1; count <= 100; count++) {
+//   if (count % 2 !== 0) {
+//     console.log("This is an odd number: ", count);
+//   }
+// }
+
+// Assignment
+// Expected output: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60,............
+
+// assignment
+for (let count = 0; count <= 100; count++) {
+  if (count % 5 == 0) {
+    // console.log("This is an odd number: ", count);
+  }
+}
+
+// assignment 222222222222222222222222222222222222
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 == 0) {
+        // console.log("This is the expected number: ", i);
+    }
+} 
+
+// 3333333333333333333333333333333333333333333333
+// for (let i=0; i <= 100; i += 5)
+//     console.log(i)
+
+
+// assignmet again
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 == 0) {
+    console.log("Vamos: ", i)
+  }
+}
